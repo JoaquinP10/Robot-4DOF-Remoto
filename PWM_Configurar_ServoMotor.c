ecf4ad8308a36a17d47b3c7061b21b31b134bdb1
Usuario Genérico Aulas - Libres <aml@pucp.edu.pe>
2023-05-26 12:51:46 -0500

    Cambios en PWM_Configurar_Servomotor. Se entiende que M0PWM0 y M0PWM1 son controlados por Generador 0, y los otros por generador 2. Se asignan los registros de acuerdo a los valores que nos brinda la datasheet del Tiva TM4C123GH6PM PWMx_y_: x es el numero de generador, y es el numero del modulo

--- PWM_Configurar_ServoMotor.c
+++ PWM_Configurar_ServoMotor.c
@@ -1,14 +1,18 @@
 /**************************************************************************
  Funcion PWM_Configurar_ServoMotor
- Descripcion: Configura el modulo M0 PWM1 para el control de los servomotores
+ Descripcion: Configura el modulo M0 PWM1.4 para el control de los servomotores
  en los pines PB4, PB5, PB6 y PB7.
  Se establece una frecuencia de Hz y un ciclo de trabajo de % para la posicion
   minima del servomotor y % para la posicion maxima
  Entradas: Ninguna
  Salidas: Ninguna
- Autor:
- Ultima modificacion: 25 de mayo de 2023
+ Autor: Joaquin Pozo
+ Ultima modificacion: 26 de mayo de 2023
 ***************************************************************************/
+//PB6 M0PWM0 M0 PWM Gen 0
+//PB7 M0PWM1 M0 PWM Gen 0
+//PB4 M0PWM2 M0 PWM Gen 1
+//PB5 M0PWM3 M0 PWM Gen 1
 
 #include "tm4c123gh6pm.h"
 #include <stdint.h>
@@ -16,8 +20,8 @@
 
 void PWM_Configurar_ServoMotor(void){
     
-    SYSCTL_RCGCGPIO_R = 0xF0;
-    while((SYSCTL_PRGPIO_R & 0xF0) == 0);
+    SYSCTL_RCGCGPIO_R = 0x02; //Habilita senhal de reloj en puerto B
+    while((SYSCTL_PRGPIO_R & 0x02) == 0); //Espera hasta que este listo
 
     GPIO_PORTB_AMSEL_R &= ~0xF0; //Desactivar modo analogico
     GPIO_PORTB_DIR_R |= 0xF0; //PB4.7 como salidas
@@ -25,25 +29,39 @@ void PWM_Configurar_ServoMotor(void){
     GPIO_PORTB_AFSEL_R |= 0xF0; //habilitar funcion alternativa
     GPIO_PORTB_PCTL_R = (GPIO_PORTB_PCTL_R & ~0xFFFF0000) | (0x44440000); //configurar como PWM
 
-    SYSCTL_RCGCPWM_R |= 0x01; //Habilita senal de reloj
-    while((SYSCTL_PRPWM_R & 0x01) == 0); //espera hasta que este listo
+    SYSCTL_RCGC0_R |= (1<<20); //Habilita senal de reloj de PWM
     
     // Configuramos divisor de frecuencia con 4
     SYSCTL_RCC_R |= SYSCTL_RCC_USEPWMDIV;
     SYSCTL_RCC_R &= ~SYSCTL_RCC_PWMDIV_M;
     SYSCTL_RCC_R |= SYSCTL_RCC_PWMDIV_4;
 
-    // Generador 0: Modo cuenta descendente, inhabilitado
+    // Generador 0 1: Modo cuenta descendente, inhabilitado
     PWM0_0_CTL_R = 0;
+    PWM1_0_CTL_R = 0;
+
     // PWM bajo en load, alto en cmpA
     PWM0_0_GENA_R = 0xC8;
+    PWM0_0_GENB_R = 0xC08;
+    PWM1_0_GENA_R = 0xC8;
+    PWM0_0_GENB_R = 0xC08;
+
     // frecuencia base = 
     PWM0_0_LOAD_R = ;
+    PWM1_0_LOAD_R = ;
+
     // Ciclo de trabajo = 
     PWM0_0_CMPA_R = ;
-    // Inicializamos PWM0
-    PWM0_0_CTL_R |= PWM_0_CTL_ENABLE;
-    // Habilitamos la senal PWM0
-    PWM0_ENABLE_R |= PWM_ENABLE_PWM0EN;
+    PWM0_0_CMPB_R = ;
+    PWM1_0_CMPA_R = ;
+    PWM1_0_CMPB_R = ;
+
+    // Inicializamos PWM0 PWM1
+    PWM0_0_CTL_R |= 0x01;
+    PWM1_0_CTL_R |= 0x03;
+    
+    // Habilitamos la senal PWM0 PWM1
+    PWM0_ENABLE_R |= 0x03; //PWM0EN PWM1EN
+    PWM1_ENABLE_R |= 0x0C; //PWM2EN PWM3EN
 
 }
