f5f95634dd839f03c56e49ef356726ac029e7e9a
JoaquinP10 <121043196+JoaquinP10@users.noreply.github.com>
2023-06-13 16:54:13 -0500

    Update Servo_Rotacion.c
    Agrega condiciones de protección. Agrega cabecera.

--- Servo_Rotacion.c
+++ Servo_Rotacion.c
@@ -1,35 +1,45 @@
-
-//Servo 1 
-//Servo 2
-//Servo 3
-//Servo 4
+/**************************************************************************
+ Funcion Servo_Rotacion
+ Descripcion: Genera un giro en uno de los servos en un angulo entre
+ 0 y 180 grados aproximadamente
+ Entradas: Servo a efectuar el giro, angulo de giro
+ Salidas: Ninguna
+ Autor: Joaquin Pozo
+ Ultima modificacion: 13 de junio de 2023
+***************************************************************************/
 
 #include "tm4c123gh6pm.h"
 #include <stdint.h>
-#include "SERVO.h"
 
 #define LOAD 40000
-#define SERVO1 1
-#define SERVO2 2
-#define SERVO3 3
-#define SERVO4 4
+#define SERVO1 1 //PB6
+#define SERVO2 2 //PB7
+#define SERVO3 3 //PB4
+#define SERVO4 4 //PB5
 
 void Servo_Rotacion(uint8_t Servo, uint16_t angulo){ //angulo de 0 a 180
-    double duty_cycle = angulo/double(36) + 5; //relacion entre angulo de rotacion y ciclo de trabajo
-    if (Servo == SERVO1){
-        PWM0_0_CMPA_R = (LOAD * duty_cycle)/100 - 1; 
+    double duty_cycle = angulo/(double)36 + 5; //relacion entre angulo de rotacion y ciclo de trabajo
+
+    if (((Servo >= SERVO1) && (Servo <= SERVO4)) && ((angulo >= 0) && (Servo <= 180))){
+        if (Servo == SERVO1){
+            PWM0_0_CMPA_R = (LOAD * duty_cycle)/100 - 1;
+        }
+        else if (Servo == SERVO2){
+            PWM0_0_CMPB_R = (LOAD * duty_cycle)/100 - 1;
+        }
+        else if (Servo == SERVO3){
+            PWM0_1_CMPA_R = (LOAD * duty_cycle)/100 - 1;
+        }
+        else if (Servo == SERVO4){
+            PWM0_1_CMPB_R = (LOAD * duty_cycle)/100 - 1;
+        }
+        else{ //you shouldn't be here
+            while(1);
+        }
     }
-    else if (Servo == SERVO2){
-        PWM0_0_CMPB_R = (LOAD * duty_cycle)/100 - 1;
-    }
-    else if (Servo == SERVO3){
-        PWM0_1_CMPA_R = (LOAD * duty_cycle)/100 - 1;
-    }
-    else if (Servo == SERVO4){
-        PWM0_1_CMPB_R = (LOAD * duty_cycle)/100 - 1;
-    }
-    else{
-        //you shouldn't be here
-    }
-    
+}
+
+void main(void){
+    Servo_Rotacion(SERVO3, 180);
+    while(1);
 }
