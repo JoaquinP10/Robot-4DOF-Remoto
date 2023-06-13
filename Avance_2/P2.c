6d6612eb2ce8de098a7efc630cc519d921230883
JoaquinP10 <121043196+JoaquinP10@users.noreply.github.com>
2023-06-13 16:35:15 -0500

    Update P2.c
    Config SW1 and SW2 with registers. Add some features like leds to code.

--- Avance_2/P2.c
+++ Avance_2/P2.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 #include "tm4c123gh6pm.h"
-#include "TivaES.h"
+#include"TivaES.h"
 
 
 #define FREQUENCY 50 //Hz
@@ -10,25 +10,37 @@
 
 void delay(uint32_t cuenta);
 void PWM_Configurar_Servomotor(void);
+void config_sw1_sw2(void);
 
 void main(void){
-
-    uint32_t i =0;
-    double duty_cycle=5;
-    uint8_t estado;
-    TivaES_Inicializa();
+    double duty_cycle=7.5;
     PWM_Configurar_Servomotor();
+    TivaES_Inicializa();
     while(1){
-	PWM0_0_CMPA_R = (40000 * duty_cycle)/100 - 1;
-        TivaES_Espera_Pulsadores();
-	if (estado==1){
-	    if ((10 - duty_cycle) < 0.1) duty_cycle+=0.1;
-	    delay(10000);
-	}
-	else if (estado==2){
-	    if ((duty_cycle-5) < 0.1) duty_cycle-=0.1;
-	    delay(10000);
-	}
+        PWM0_0_CMPA_R = (40000 * duty_cycle)/100 - 1;
+        if ((GPIO_PORTF_DATA_R & 0x10) == 0){
+            while((GPIO_PORTF_DATA_R & 0x10) == 0);
+            if ((10 - duty_cycle) > 0.1) {
+                duty_cycle+=0.1;
+                TivaES_LedColor(ROJO);
+            }
+            else{
+                TivaES_LedColor(NEGRO);
+            }
+            delay(10000);
+
+        }
+        else if ((GPIO_PORTF_DATA_R & 0x01) == 0){
+            while((GPIO_PORTF_DATA_R & 0x01) == 0);
+            if ((duty_cycle-5) > 0.1){
+                duty_cycle-=0.1;
+                TivaES_LedColor(AZUL);
+            }
+            else{
+                TivaES_LedColor(NEGRO);
+            }
+            delay(10000);
+        }
     }
 
 }
@@ -38,9 +50,24 @@ void delay(uint32_t cuenta){
     for(i=0; i<cuenta; i++);
 }
 
+void config_sw1_sw2(void)
+{
+    // activamos la señal de reloj del puerto F
+    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R5;
+    // espera a que el puerto este listo para operar
+    while ((SYSCTL_PRGPIO_R & SYSCTL_PRGPIO_R5) == 0){}
+    GPIO_PORTF_LOCK_R = 0x4C4F434B; // Desbloquear puerto F
+    GPIO_PORTF_CR_R |= 0x01;        // Se habilita PF0 (SW2)
+    //SW1 está conectado al pin PF4, SW2 al pin PF0.
+    GPIO_PORTF_DIR_R &= ~(0x11);    // Configura el pin 0 (bit0) y pin 4 (bit4) como entrada
+    GPIO_PORTF_AFSEL_R &= ~(0x11);  // No utilizar funciones alternas
+    GPIO_PORTF_PUR_R |= 0x11;       // Activa resistencias de pull-up de los pines 0 y 4
+    GPIO_PORTF_DEN_R |= 0x11;       // Habilita los niveles digitales de los pines pin 0 y 4
+} // fin config_sw1_sw2
+
 void PWM_Configurar_Servomotor(void){
 
-    SYSCTL_RCGCGPIO_R = 0x02; //Habilita senhal de reloj en puerto B
+    SYSCTL_RCGCGPIO_R |= 0x02; //Habilita senhal de reloj en puerto B
     while((SYSCTL_PRGPIO_R & 0x02) == 0); //Espera hasta que este listo
 
     GPIO_PORTB_AMSEL_R &= ~0xF0; //Desactivar modo analogico
@@ -85,6 +112,7 @@ void PWM_Configurar_Servomotor(void){
     PWM0_ENABLE_R |= 0x0C; //PWM2EN PWM3EN
 
 }
+
 
 
 
