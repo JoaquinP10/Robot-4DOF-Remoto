de1f2b165122cab2eeeb238efe8abb6c3687c7cf
bro@gmail.com <bro@gmail.com>
2023-06-19 13:43:56 -0500

    Little tweaks to Servos_Rotacion.c

--- Servos_Rotacion.c
+++ Servos_Rotacion.c
@@ -19,7 +19,7 @@
 
 void Servo_Rotacion(double duty_cycle1, double duty_cycle2, double duty_cycle3, double duty_cycle4){ //angulo de 0 a 180
     
-    if ((duty_cycle >= 5) && (duty_cycle <= 10)){
+    if (((duty_cycle1 >= 5) && (duty_cycle1 <= 10)) && ((duty_cycle2 >= 5) && (duty_cycle2 <= 10)) && ((duty_cycle3 >= 5) && (duty_cycle3 <= 10)) && ((duty_cycle4 >= 5) && (duty_cycle4 <= 10))){
 		PWM0_0_CMPA_R = (LOAD * duty_cycle1)/100 - 1; //SERVO1
 		PWM0_0_CMPB_R = (LOAD * duty_cycle2)/100 - 1; //SERVO2
 		PWM0_1_CMPA_R = (LOAD * duty_cycle3)/100 - 1; //SERVO3
